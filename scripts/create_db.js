b8671d9db8fce1b0898679247d974480a0af5f77
Charlie Arrendale <carrendale@gmail.com>
2017-12-02 12:39:40 -0500

    WIP

--- scripts/create_db.js
+++ scripts/create_db.js
@@ -19,20 +19,6 @@ CREATE TABLE IF NOT EXISTS`' + dbconfig.database + '`.`' + dbconfig.users_table 
     UNIQUE INDEX `username_UNIQUE` (`username` ASC) \
 )');
 
-// connection.query('\
-// CREATE TABLE IF NOT EXISTS`' + dbconfig.database + '`.`' + dbconfig.orders_table + '` ( \
-//     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
-//     `username` VARCHAR(20) NOT NULL, \
-//     `password` CHAR(60) NOT NULL, \
-//     `user_first_name` VARCHAR(255) NOT NULL, \
-//     `user_last_name` VARCHAR(255) NOT NULL, \
-//     `user_number` INT(15) NOT NULL, \
-//     `user_email` VARCHAR(255) NOT NULL, \
-//         PRIMARY KEY (`id`), \
-//     UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
-//     UNIQUE INDEX `username_UNIQUE` (`username` ASC) \
-// )');
-
 console.log('Success: Database Created!');
 
 connection.end();

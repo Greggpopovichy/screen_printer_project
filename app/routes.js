7b2aa6863df3b268389984aab95f4da1265825b8
Nicola Limbach <limbach.nicola@gmail.com>
2017-12-04 20:02:04 -0500

    test

--- app/routes.js
+++ app/routes.js
@@ -53,7 +53,7 @@ module.exports = function(app, passport) {
 
     // process the login form
     app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/checkauth', // redirect to the secure profile section
+            successRedirect : '/profile', // redirect to the secure profile section
             failureRedirect : '/about', // redirect back to the signup page if there is an error
             failureFlash : true // allow flash messages
         }),

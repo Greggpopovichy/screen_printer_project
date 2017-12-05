22788144db95bc0cb0e723f58ce5c1d414a57140
Charlie Arrendale <carrendale@gmail.com>
2017-12-05 17:20:55 -0500

    final?

--- app/routes.js
+++ app/routes.js
@@ -51,7 +51,7 @@ module.exports = function(app, passport) {
         res.render('login', { user : req.user, message: req.flash('loginMessage') });
     });
 
-    // process the login form
+    // process the login form.
     app.post('/login', passport.authenticate('local-login', {
             successRedirect : '/profile', // redirect to the secure profile section
             failureRedirect : '/login', // redirect back to the signup page if there is an error
--- config/passport.js
+++ config/passport.js
@@ -16,12 +16,11 @@ module.exports = function(passport) {
     // required for persistent login sessions
     // passport needs ability to serialize and unserialize users out of session
 
-    // used to serialize the user for the session
+    // used to save the user in session and terminate the users session
     passport.serializeUser(function(user, done) {
         done(null, user.id);
     });
 
-    // used to deserialize the user
     passport.deserializeUser(function(id, done) {
         connection.query("SELECT * FROM users WHERE id = ? ",[id], function(err, rows){
             done(err, rows[0]);

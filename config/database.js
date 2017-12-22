04e0b47ecf579e1fefcbd67bc30c8f6a7f1c921e
Charlie Arrendale <carrendale@gmail.com>
2017-12-21 19:18:59 -0500

    test

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
     'connection': {
-        'host': 'localhost',
-        'user': 'root',
-        'password': ''
+        'host': 'us-cdbr-iron-east-05.cleardb.net',
+        'user': 'bf6098f7b32424',
+        'password': '9dcf4705'
     },
-    'database': 'screen_printer',
+    'database': 'heroku_36fbcd957a0aafb',
     'users_table': 'users',
     'orders_table': 'orders'
 };
--- server.js
+++ server.js
@@ -44,6 +44,8 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
+//remote db connection
+
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
